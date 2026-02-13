ecbfe4cc2afefaf1645d521c1dec249c614cb7b2
azimthariq9 <azimthariq9@gmail.com>
2026-02-13 19:50:59 +0700

    feat: add create item endpoint

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
 items = []
 
+class Item(BaseModel):
+    id: int
+    name: str
+    stock: int
+
+@app.get("/")
+def root():
+    return {"message": "Inventory API Running"}
+
 @app.get("/items")
 def list_items():
     return items
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return item
